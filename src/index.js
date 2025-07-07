77f834f1c3b73806341e096908d5995e0ba7ca74
finntastic <179311210+finntaztic@users.noreply.github.com>
2025-07-07 13:32:14 +0800

    create eventlistener to option

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import "./styles.css";
+
+
+
+const option = document.querySelectorAll('.option');
+
+console.log(option);
+
+// option.addEventListener('click', function (){
+//     console.log('hello');
+// });
+
+// option.forEach(option => {
+//     option.addEventListener('mousehover', function(){
+//         console.log('hello');
+//     })
+// });
